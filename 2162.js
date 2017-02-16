fad2105b0e9b0fa5731702fe92979434ec40d968
KarenKwz <kwz-19@hotmail.com>
2017-02-16 13:47:48 -0600

    Update 2162.js

--- 2162.js
+++ 2162.js
@@ -1,4 +1,4 @@
 var miCanvas=document.getElementById("miCanvas");
 var contexto=miCanvas.getContext("2d");
 contexto.fillStyle="red";
-contexto.fillRect(300, 300, 500, 500);
+contexto.fillRect(30, 30, 50, 50);
