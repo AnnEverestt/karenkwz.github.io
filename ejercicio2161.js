77057e1889e6401e8ade44fe491516b72196d7fd
KarenKwz <kwz-19@hotmail.com>
2017-02-16 14:28:02 -0600

    Update ejercicio2161.js

--- ejercicio2161.js
+++ ejercicio2161.js
@@ -1,10 +1,5 @@
 
-var miCanvas=document.getElementById("miCanvas");
-var contexto=miCanvas.getContext("2d");
-contexto.fillStyle="red";
-contexto.fillRect(30, 30, 50, 50);
-
-var mioCanvas=document.getElementById("mioCanvas");
-var contexto=mioCanvas.getContext("2d");
+var miCanvas1=document.getElementById("miCanvas1");
+var contexto=miCanvas1.getContext("2d");
 contexto.fillStyle="blue";
 contexto.fillRect(50, 30, 100, 50);
