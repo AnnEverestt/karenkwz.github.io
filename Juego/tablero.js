e15ee48efc8c5112437ec1c8d9bc8b6be0ac3a71
KarenKwz <kwz-19@hotmail.com>
2017-02-23 13:58:33 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -1,37 +1,21 @@
-var forma = new THREE.Geometry();
-forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); 
-forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); 
-forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); 
-forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); 
+var figura = new THREE.Shape();
 
-forma.vertices.push( new THREE.Vector3( -1,  1,  1 ) ); 
-forma.vertices.push( new THREE.Vector3( -1,  1,  -1 ) );
-forma.vertices.push( new THREE.Vector3( 1,  1,  -1 ) );
-forma.vertices.push( new THREE.Vector3( 1,  1,  1 ) );
-
-forma.faces.push( new THREE.Face3( 0, 1, 3 ) ); 
-forma.faces.push( new THREE.Face3( 3, 1, 2 ) ); 
-forma.faces.push( new THREE.Face3( 2, 3, 5 ) ); 
-forma.faces.push( new THREE.Face3( 5, 4, 3 ) );
-forma.faces.push( new THREE.Face3( 3, 0, 7 ) ); 
-forma.faces.push( new THREE.Face3( 7, 4, 3 ) ); 
-forma.faces.push( new THREE.Face3( 7, 0, 1 ) ); 
-forma.faces.push( new THREE.Face3( 1, 7, 6 ) ); 
-forma.faces.push( new THREE.Face3( 6, 2, 5 ) ); 
-forma.faces.push( new THREE.Face3( 2, 1, 6 ) ); 
-forma.faces.push( new THREE.Face3( 6, 5, 4 ) ); 
-forma.faces.push( new THREE.Face3( 4, 7, 6 ) ); 
-forma.computeBoundingSphere();
-forma.computeFaceNormals();
+figura.moveTo(10, 10);
+figura.lineTo(10, 40);
+figura.lineTo(40, 40);
+figura.lineTo(10, 10);
+figura.lineTo(40, 10);
+figura.lineTo(10, 10);
+var forma = new THREE.ExtrudeGeometry( figura,{amount: 10} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
+malla.rotateY( Math.PI/4 );
 var escena = new THREE.Scene();
-escena.add( malla );
-var escena = new THREE.Scene();
-escena.add( malla );
+escena.add(malla);
+
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 500;
+
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
