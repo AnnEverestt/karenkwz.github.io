71e3960982351dc66302a56ad67c724a75b26fe1
KarenKwz <kwz-19@hotmail.com>
2017-02-28 14:06:04 -0600

    Update gus.js

--- Juego/gus.js
+++ Juego/gus.js
@@ -1,6 +1,6 @@
 var puntos = [];
 for ( var i = 0; i < 50; i ++ ) {
-    puntos.push( new THREE.Vector2(Math.sin( i * 0.5), ( i - 5 ) * 2 ) );
+    puntos.push( new THREE.Vector2(Math.sin( i * 0.5), ( i ) * 2 ) );
 }
 var forma = new THREE.LatheGeometry(puntos);
 
