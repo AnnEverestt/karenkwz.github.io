5d5ee768ebcb876aa71ff9db944ea655fa762ebd
KarenKwz <kwz-19@hotmail.com>
2017-02-28 09:36:44 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -19,7 +19,7 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 100;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+renderizador.setSize( window.innerHeight*1, window.innerHeight*1 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
 
