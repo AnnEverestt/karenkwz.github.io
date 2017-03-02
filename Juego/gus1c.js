9a5c5c2bbb235304fae48600b600a5f126361e82
KarenKwz <kwz-19@hotmail.com>
2017-03-02 11:10:38 -0600

    Update gus1c.js

--- Juego/gus1c.js
+++ Juego/gus1c.js
@@ -1,8 +1,23 @@
-var points = [];
-for ( var i = 0; i < 10; i ++ ) {
-	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+var puntos = [];
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(Math.cos( i * 0.5), ( i) * 2 ) );
 }
-var geometry = new THREE.LatheGeometry( points );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-var lathe = new THREE.Mesh( geometry, material );
-scene.add( lathe );
+var forma = new THREE.LatheGeometry(puntos);
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
+
+
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
