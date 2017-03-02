93bb12c86b173088122f8f1838db5c47f717bdd2
KarenKwz <kwz-19@hotmail.com>
2017-03-02 11:04:15 -0600

    Update gus1c.js

--- Juego/gus1c.js
+++ Juego/gus1c.js
@@ -1,25 +1,8 @@
-var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
-var esferaForma = new THREE.SphereGeometry(.65);
-esferaForma.translate(0,1,0);
-
-var troncoMalla = new THREE.Mesh(troncoForma);
-var esferaMalla = new THREE.Mesh(esferaForma);
-
-var arbolForma = new THREE.Geometry();
-arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
-arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
-
-var material = new THREE.MeshNormalMaterial();
-var arbolMalla = new THREE.Mesh(arbolForma, material);
-
-
-var escena = new THREE.Scene();
-escena.add(arbolMalla);
-
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-
-renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+var points = [];
+for ( var i = 0; i < 10; i ++ ) {
+	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+}
+var geometry = new THREE.LatheGeometry( points );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var lathe = new THREE.Mesh( geometry, material );
+scene.add( lathe );
