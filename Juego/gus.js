a45eb427a4168f37cabbeac4d23600792741e0a8
KarenKwz <kwz-19@hotmail.com>
2017-03-02 10:24:09 -0600

    Update gus.js

--- Juego/gus.js
+++ Juego/gus.js
@@ -3,13 +3,15 @@ for ( var i = 0; i < 50; i ++ ) {
     puntos.push( new THREE.Vector2(Math.cos( i * 0.5)*15+30, ( i -5) * 2 ) );
 }
 var forma = new THREE.LatheGeometry(puntos);
-
 var material = new THREE.MeshNormalMaterial();
-
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/6 );
 var escena = new THREE.Scene();
 escena.add(malla);
+
+var geometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
+var material = new THREE.MeshBasicMaterial();
+var sphere = new THREE.Mesh( geometry, material );
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
