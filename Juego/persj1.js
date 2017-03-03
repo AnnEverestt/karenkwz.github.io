361ee85fa0bf4715966b8f3528e1038535b20963
KarenKwz <kwz-19@hotmail.com>
2017-03-03 10:32:58 -0600

    Update persj1.js

--- Juego/persj1.js
+++ Juego/persj1.js
@@ -1,4 +1,5 @@
 var forma=new THREE.Geometry();
+//cuerpo VERTICES
 forma.vertices.push( new THREE.Vector3( 50,  0,  50 ) ); // Vértice 0
 forma.vertices.push( new THREE.Vector3( 50,  0, -50 ) ); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-50,  0, -50 ) ); // Vértice 2
@@ -8,8 +9,18 @@ forma.vertices.push( new THREE.Vector3( 50, 150, -50 ) ); // Vértice 5
 forma.vertices.push( new THREE.Vector3(-50, 150, -50 ) ); // Vértice 6
 forma.vertices.push( new THREE.Vector3(-50, 150,  50 ) ); // Vértice 7
 
+//brazo VERTICES
+forma.vertices.push( new THREE.Vector3( 50,  0,  25 ) ); // Vértice 8--0
+forma.vertices.push( new THREE.Vector3( 50,  0, -25 ) ); // Vértice 9---1
+forma.vertices.push( new THREE.Vector3(-75,  0, -25 ) ); // Vértice 10---2
+forma.vertices.push( new THREE.Vector3(-75,  0,  25 ) ); // Vértice 11---3
+forma.vertices.push( new THREE.Vector3( 50, 50,  25 ) ); // Vértice 12---4
+forma.vertices.push( new THREE.Vector3( 50, 50, -25 ) ); // Vértice 13---5
+forma.vertices.push( new THREE.Vector3(-75, 50, -25 ) ); // Vértice 14---6
+forma.vertices.push( new THREE.Vector3(-75, 50,  25 ) ); // Vértice 15---7
 
 
+//cuerpo CARAS
 forma.faces.push( new THREE.Face3( 0, 1, 2 ) ); // Cara 0
 forma.faces.push( new THREE.Face3( 2, 3, 0 ) ); // Cara 1
 forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
@@ -22,6 +33,21 @@ forma.faces.push( new THREE.Face3( 2, 6, 5 ) );
 forma.faces.push( new THREE.Face3( 5, 1, 2 ) );
 forma.faces.push( new THREE.Face3( 4, 5, 6 ) );
 forma.faces.push( new THREE.Face3( 6, 7, 4 ) );
+
+//brazo CARAS
+
+forma.faces.push( new THREE.Face3( 8, 9,10 ) ); // Cara 0
+forma.faces.push( new THREE.Face3( 10, 11, 8) ); // Cara 1
+forma.faces.push( new THREE.Face3( 8, 9, 13 ) );
+forma.faces.push( new THREE.Face3( 13, 12, 8 ) );
+forma.faces.push( new THREE.Face3( 8, 12, 15 ) );
+forma.faces.push( new THREE.Face3( 15, 11, 8 ) );
+forma.faces.push( new THREE.Face3( 11, 15, 14 ) );
+forma.faces.push( new THREE.Face3( 14, 10, 11 ) );
+forma.faces.push( new THREE.Face3( 10, 14, 13 ) );
+forma.faces.push( new THREE.Face3( 13, 9, 10 ) );
+forma.faces.push( new THREE.Face3( 12, 13, 14 ) );
+forma.faces.push( new THREE.Face3( 14, 15, 12 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
