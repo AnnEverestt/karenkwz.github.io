09e75a730194fd0ac272feaba69dbb5dad0099be
KarenKwz <kwz-19@hotmail.com>
2017-03-02 22:10:39 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -1,12 +1,12 @@
 var figura = new THREE.Shape();
 
 figura.moveTo(0, 0);
-figura.lineTo(0, 50);
-figura.lineTo(50, 50);
-figura.lineTo(50, 0);
+figura.lineTo(0, 500);
+figura.lineTo(500, 500);
+figura.lineTo(500, 0);
 figura.lineTo(0, 0);
 
-var forma = new THREE.ExtrudeGeometry( figura,{amount: 1} );
+var forma = new THREE.ExtrudeGeometry( figura,{amount: 10} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI*(3/4) );
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 250;
+camara.position.z = 750;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
