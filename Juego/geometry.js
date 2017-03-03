46c99c241dc3b3cb0cff9336a27410cbba861def
KarenKwz <kwz-19@hotmail.com>
2017-03-02 23:31:19 -0600

    Update geometry.js

--- Juego/geometry.js
+++ Juego/geometry.js
@@ -1,4 +1,4 @@
-var troncoForma = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var geometry = new THREE.ConeGeometry( 20, 50, 32 );
 var esferaForma = new THREE.SphereGeometry(25);
 esferaForma.translate(0,1,0);
 
