074ff3c12273ff2f858bbeedde0a68c07788f2f7
KarenKwz <kwz-19@hotmail.com>
2017-03-02 22:31:39 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -1,15 +1,15 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(0, 0);
-figura.lineTo(0, 100);
+figura.moveTo(-10, -10);
+figura.lineTo(-10, 100);
 figura.lineTo(100, 100);
-figura.lineTo(100, 0);
-figura.lineTo(0, 0);
+figura.lineTo(100,-10);
+figura.lineTo(-10, -10);
 
 var forma = new THREE.ExtrudeGeometry( figura,{amount: 10} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/4 );
+malla.rotateX( Math.PI*(3/4));
 var escena = new THREE.Scene();
 escena.add(malla);
 
