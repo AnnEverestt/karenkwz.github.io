14f76b3d7f5d26e16ac03d8e13c4d7e6a9b12466
KarenKwz <kwz-19@hotmail.com>
2017-03-05 03:47:42 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -53,14 +53,7 @@ var forma5 = new THREE.LatheGeometry(puntos5);
 var material5 = new THREE.MeshNormalMaterial();
 var malla5 = new THREE.Mesh( forma5, material5 );
 
-var puntos6 = [];
-for ( var j = 0; j < 50; j ++ ) {
-    puntos6.push( new THREE.Vector2(Math.sin( j * 0.2) * 15 + 50,j));
-}
-puntos6.translate(-400,80,-200);                
-var forma6 = new THREE.LatheGeometry(puntos6);
-var material6 = new THREE.MeshNormalMaterial();
-var malla6 = new THREE.Mesh( forma6, material6 );
+
 
 //ESCENA
 var escena = new THREE.Scene();
@@ -68,7 +61,7 @@ escena.add(malla1); //TABLERO
 escena.add(forma3Malla);//UNIONMMALLA1
 escena.add(forma4Malla);//UNIONMALLA2
 escena.add(malla5);//REVOLUCION
-escena.add(malla6);//REVOLUCION
+
 
         
         
