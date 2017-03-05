96a5a13bd0b7dff82964c52d2df974812dd32e9c
KarenKwz <kwz-19@hotmail.com>
2017-03-05 04:19:47 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -51,7 +51,7 @@ escena.add(malla1); //TABLERO
 escena.add(forma3Malla);//UNIONMMALLA1
 escena.add(forma4Malla);//UNIONMALLA2
         
-malla1.rotateX(Math.PI*(3/4));//TABLERO
+malla1.rotateX(Math.PI);//TABLERO
 
 
 var camara = new THREE.PerspectiveCamera();
