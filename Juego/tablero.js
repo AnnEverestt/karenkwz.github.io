7edf489b67b72c8dec98eb5c6b5b6146b26858c6
KarenKwz <kwz-19@hotmail.com>
2017-03-05 02:41:09 -0600

    Update tablero.js

--- Juego/tablero.js
+++ Juego/tablero.js
@@ -12,10 +12,10 @@ var malla1 = new THREE.Mesh( forma1, material1 );
 //UNION DE MALLAS
 //MALLA1 
 var aspasForma = new THREE.TorusKnotGeometry( 50, 3, 100, 16 );
-aspasForma.translate(-300,80,-300);
+aspasForma.translate(-350,80,-350);
 var esferaForma = new THREE.SphereGeometry(25);
 esferaForma.translate(-350,80,-350);
-var toroForma = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var toroForma = new THREE.TorusGeometry( 30, 3, 16, 100 );
 toroForma.translate(-350,80,-350);
 var aspasMalla = new THREE.Mesh(aspasForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
@@ -28,11 +28,11 @@ var material3 = new THREE.MeshNormalMaterial();
 var forma3Malla = new THREE.Mesh(forma3, material3);
 //MALLA2
 var aspasForma4 = new THREE.TorusKnotGeometry( 50, 3, 100, 16 );
-aspasForma4.translate(300,80,-300);
+aspasForma4.translate(350,80,-350);
 var esferaForma4 = new THREE.SphereGeometry(25);
-esferaForma4.translate(300,80,-300);
-var toroForma4 = new THREE.TorusGeometry( 10, 3, 16, 100 );
-toroForma4.translate(-300,100,-300);
+esferaForma4.translate(350,80,-350);
+var toroForma4 = new THREE.TorusGeometry( 30, 3, 16, 100 );
+toroForma4.translate(-350,100,-350);
 var aspasMalla4 = new THREE.Mesh(aspasForma4);
 var esferaMalla4 = new THREE.Mesh(esferaForma4);
 var toroMalla4= new THREE.Mesh(toroForma4)
@@ -58,8 +58,8 @@ var material2 = new THREE.MeshNormalMaterial();
 var malla2 = new THREE.Mesh( forma2, material2 );
 var material5 = new THREE.MeshNormalMaterial();
 var malla5 = new THREE.Mesh( forma5, material5 );
-malla2.translate(-300,20,-200);
-malla5.translate(-300,50,-200);
+malla2.translate(-350,50,-200);
+malla5.translate(-350,50,-200);
 
 
 //ESCENA
