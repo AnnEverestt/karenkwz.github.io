f71179d40fc86f2205a9050337e963cf9103c75d
KarenKwz <kwz-19@hotmail.com>
2017-03-05 20:18:07 -0600

    Update extrusion.js

--- Juego/extrusion.js
+++ Juego/extrusion.js
@@ -8,8 +8,6 @@ figura8.lineTo(-20, -20);
 var forma8 = new THREE.ExtrudeGeometry( figura8,{amount: 100} );
 var material8 = new THREE.MeshNormalMaterial();
 var malla8 = new THREE.Mesh( forma8, material8 );
-malla8.translate(-250,250,-100);
-
 
 var escena = new THREE.Scene();
 escena.add(malla8); //TABLERO
@@ -17,7 +15,7 @@ malla8.rotateX(Math.PI/4);
 
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 200;
+camara.position.z = 100;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
