336a847cb0e9b531887e6ec930e3ed547a941807
KarenKwz <kwz-19@hotmail.com>
2017-03-05 19:55:20 -0600

    Update revolucion.js

--- Juego/revolucion.js
+++ Juego/revolucion.js
@@ -1,6 +1,6 @@
 var puntos = [];
-for ( var i = 0; i < 50; i ++ ) {
-    puntos.push( new THREE.Vector2(Math.cos( i *2)*20+50, ( i) * 4) );
+for ( var i = 0; i < 30; i ++ ) {
+    puntos.push( new THREE.Vector2(Math.cos( i *2)*20+50,  i) );
 }
 var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
@@ -8,10 +8,22 @@ var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/6 );
 malla.rotateY( Math.PI/2 );
+var puntos2 = [];
+for ( var j = 0; j < 30; j ++ ) {
+    puntos2.push( new THREE.Vector2(Math.cos( j *0.3)*20+50, j+20) );
+}
+var forma2 = new THREE.LatheGeometry(puntos2);
+var material2 = new THREE.MeshNormalMaterial();
+
+var malla2 = new THREE.Mesh( forma2, material2 );
+malla2.rotateX( Math.PI/6 );
+malla2.rotateY( Math.PI/2 );
 
 
 var escena = new THREE.Scene();
 escena.add(malla);
+escena.add(malla2);
+
 
 
 var camara = new THREE.PerspectiveCamera();
