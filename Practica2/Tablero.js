921d45adce795499ebf3d06553d34ee254d0fbb2
KarenKwz <kwz-19@hotmail.com>
2017-03-09 13:47:09 -0600

    Update Tablero.js

--- Practica2/Tablero.js
+++ Practica2/Tablero.js
@@ -1,13 +1,13 @@
 var forma=new THREE.Geometry();
 //cuerpo VERTICES
-forma.vertices.push( new THREE.Vector3( 50,  0,  50 ) ); // Vértice 0
-forma.vertices.push( new THREE.Vector3( 50,  0, -50 ) ); // Vértice 1
-forma.vertices.push( new THREE.Vector3(-50,  0, -50 ) ); // Vértice 2
-forma.vertices.push( new THREE.Vector3(-50,  0,  50 ) ); // Vértice 3
-forma.vertices.push( new THREE.Vector3( 50, 5,  50 ) ); // Vértice 4
-forma.vertices.push( new THREE.Vector3( 50, 5, -50 ) ); // Vértice 5
-forma.vertices.push( new THREE.Vector3(-50, 5, -50 ) ); // Vértice 6
-forma.vertices.push( new THREE.Vector3(-50, 5,  50 ) ); // Vértice 7
+forma.vertices.push( new THREE.Vector3( 100,  0,  100 ) ); // Vértice 0
+forma.vertices.push( new THREE.Vector3( 100,  0, -100 ) ); // Vértice 1
+forma.vertices.push( new THREE.Vector3(-100,  0, -100 ) ); // Vértice 2
+forma.vertices.push( new THREE.Vector3(-100,  0,  100 ) ); // Vértice 3
+forma.vertices.push( new THREE.Vector3( 100, 5,  100 ) ); // Vértice 4
+forma.vertices.push( new THREE.Vector3( 100, 5, -100 ) ); // Vértice 5
+forma.vertices.push( new THREE.Vector3(-100, 5, -100 ) ); // Vértice 6
+forma.vertices.push( new THREE.Vector3(-100, 5,  100 ) ); // Vértice 7
 forma.faces.push( new THREE.Face3( 0, 1, 2 ) ); // Cara 0
 forma.faces.push( new THREE.Face3( 2, 3, 0 ) ); // Cara 1
 forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
