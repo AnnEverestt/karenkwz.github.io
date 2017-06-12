4006a3c3f893e389dfe4c7e41c0341b9896881c4
KarenKwz <kwz-19@hotmail.com>
2017-06-12 00:08:20 -0600

    Create Textura.js

--- ProyectoFinal/Textura.js
+++ ProyectoFinal/Textura.js
@@ -1,9 +1,9 @@
 function Completo(){
   THREE.Object3D.call(this);
   THREE.ImageUtils.crossOrigin = '';
-  var cara = THREE.ImageUtils.loadTexture('http://karenkwz.github.io/crate.gif');
-  var brazos = THREE.ImageUtils.loadTexture('http://karenkwz.github.io/crate.gif');
-  var pies = THREE.ImageUtils.loadTexture('http://karenkwz.github.io/crate.gif');  
+  var cara = THREE.ImageUtils.loadTexture('https://github.com/KarenKwz/karenkwz.github.io/12899665_10153684992446843_1267488120_n.jpg');
+  var brazos = THREE.ImageUtils.loadTexture('https://github.com/KarenKwz/karenkwz.github.io/12899889_10153685013876843_569751267_n.jpg');
+  var pies = THREE.ImageUtils.loadTexture('https://github.com/KarenKwz/karenkwz.github.io/12919388_10153685013881843_2062601905_o.jpg');  
   
   
    
