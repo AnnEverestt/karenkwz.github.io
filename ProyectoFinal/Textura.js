3fa0a9d04f2ca22bbcaa7a1e279a91615b65509f
KarenKwz <kwz-19@hotmail.com>
2017-06-12 12:46:23 -0500

    Create Textura.js

--- ProyectoFinal/Textura.js
+++ ProyectoFinal/Textura.js
@@ -8,8 +8,8 @@ function Completo(){
   
    
 this.cuerpo = new THREE.Mesh(new THREE.SphereGeometry(3,100,100),new THREE.MeshLambertMaterial({map:cara}));
-  this.brazoI = new THREE.Mesh(new THREE.BoxGeometry(1,0.5,2),new THREE.MeshLambertMaterial({map:brazos}));
-  this.brazoD = new THREE.Mesh(new THREE.BoxGeometry(1,0.5,2),new THREE.MeshLambertMaterial({map:brazos}));
+  this.brazoI = new THREE.Mesh(new THREE.BoxGeometry(1,1,3),new THREE.MeshLambertMaterial({map:brazos}));
+  this.brazoD = new THREE.Mesh(new THREE.BoxGeometry(1,1,3),new THREE.MeshLambertMaterial({map:brazos}));
   this.pieI = new THREE.Mesh(new THREE.SphereGeometry(1,1,0.5),new THREE.MeshLambertMaterial({map:pies}));
   this.pieD = new THREE.Mesh(new THREE.SphereGeometry(1,1,0.5),new THREE.MeshLambertMaterial({map:pies}));
   
